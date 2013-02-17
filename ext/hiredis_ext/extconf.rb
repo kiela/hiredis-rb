ad15544de859b655e1fd973a8b1be11db25883c4
Kamil Kieliszczyk <kiela@heimdall.pl>
2013-02-17 22:00:38 +0100

    Determine different make utility for FreeBSD/Solaris

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -3,13 +3,15 @@ require 'mkmf'
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
+make_command = RUBY_PLATFORM.downcase =~ /bsd|solaris/ ? 'gmake' : 'make'
+
 unless File.directory?(hiredis_dir)
   STDERR.puts "vendor/hiredis missing, please checkout its submodule..."
   exit 1
 end
 
 # Make sure hiredis is built...
-system("cd #{hiredis_dir} && make static")
+system("cd #{hiredis_dir} && #{make_command} static")
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
